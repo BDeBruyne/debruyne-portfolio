0adc54c73fb14833a47324f8f3e7b1edb529d268
BDeBruyne <debruynebrad@gmail.com>
2022-11-12 00:19:25 -0700

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,28 +13,24 @@ const About = () => {
                             
                 <div>
                     <p>I am excited about gaining experience in &nbsp;<span class='special-word'>Web Development</span> 
-                        &nbsp;and improving the my skillset as I work towards becoming a professional developer.
+                       &nbsp;and improving the my skillset as I work towards becoming a professional developer.
+                    </p>
+                    <br></br>                    
+                    <p> I have experience with <span class='special-word'>Graphic Design</span> for print/web and&nbsp; 
+                        <span class='special-word'>Animation</span> using the <span class='special-word'>Adobe Creative Suite</span>,
+                        &nbsp;<span class='special-word'>Front End Development</span> using <span class='special-word'>HTML</span>,  
+                        &nbsp;<span class='special-word'>CSS</span>, <span class='special-word'>JavaScript</span>, 
+                        &nbsp;<span class='special-word'>Angular</span>&nbsp; and <span class='special-word'>React</span>, as well 
+                        as programming in &nbsp;<span class='special-word'>Back End Development</span>&nbsp; with such languages
+                        as <span class='special-word'>Java</span>,&nbsp;<span class='special-word'>C#</span> and 
+                        <span class='special-word'>.Net<span className='text-sm'>(ASP and Core)</span></span>. I also have 
+                        experience with <span class='special-word'>Database Concepts</span>, &nbsp;
+                        <span class='special-word'>Design</span> and &nbsp;<span class='special-word'>MySQL</span> allowing me to
+                        tie everything together into fully functional dynamic web applications.
                     </p><br></br>
-                    <p>In 2003 I graduated from <span class='special-word'>McKenzie College</span> after 
-                        completing their&nbsp;<span class='special-word'>Digital Media Design</span> program. 
-                        In that program I learned the fundamentals of &nbsp;<span class='special-word'>Graphic</span> and &nbsp;
-                        <span class='special-word'>Web Design</span> using the <span class='special-word'>Adobe Creative Suite</span>, 
-                        as well as <span class='special-word'>Web Development</span>&nbsp;basics using <span class='special-word'>HTML</span>,
-                         <span class='special-word'>CSS</span>, <span class='special-word'>JavaScript</span> and 
-                         &nbsp;<span class='special-word'>Flash <span class='special-word text-sm'>(Adobe Animate)</span></span>.
-                    </p><br></br>
-                    <p>I currently find myself a recent honor roll graduate of 
-                    &nbsp;<span class='special-word'>Robertson College's</span>&nbsp;&nbsp;<span class='special-word'>Full Stack Web Development</span> 
-                    &nbsp;program.
-                                             
-                        In this program I have learned the fundamentals of <span class='special-word'>
-                        Front End Development</span> using HTML, CSS, JavaScript, Angular and React, as well as programming on the 
-                        &nbsp;<span class='special-word'>Back End Development</span>&nbsp;side with such languages as <span class='special-word'>Java</span>,
-                        &nbsp;<span class='special-word'>C#</span> and <span class='special-word'>.Net<span className='text-sm'>(ASP and Core)</span></span>. 
-                        I also learned <span class='special-word'>Database Concepts</span>
-                        , &nbsp;<span class='special-word'>Design</span> and &nbsp;<span class='special-word'>MySQL</span> tying them 
-                        all together into fully functional dynamic web applications.
-                    </p><br></br>
+                    <p>When I am not at my workstation I enjoy spending time with my <span class='special-word'>family</span>, 
+                    participating in sports, chilling out with a good movie or playing video games from time to time.
+                    </p>
                  
                         
                     
